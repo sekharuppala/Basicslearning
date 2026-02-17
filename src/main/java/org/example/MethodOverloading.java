d903a3840ce39b6ec6985cbc633baeaaca184fcd
Sekhar <sekhargowd645@gmail.com>
2026-02-17 17:05:02 +0530

    Sample test

---
+++ src/main/java/org/example/MethodOverloading.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class math{
+    public void int add(int a,int b){
+        return a+b;
+    }
+
+    public double add(double a, double b,double c){
+        return a+b+c;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        math m=new math();
+
+    }
+}
