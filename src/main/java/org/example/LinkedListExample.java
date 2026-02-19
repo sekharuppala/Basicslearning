f60966c6d521038e3b2d45075abe381edee1d1c3
Sekhar <sekhargowd645@gmail.com>
2026-02-19 15:26:52 +0530

    MiniTask

---
+++ src/main/java/org/example/LinkedListExample.java
@@ -0,0 +1,32 @@
+package org.example;
+//import java.util.*;
+//public class LinkedListExample {
+//    public static void main(String[] args) {
+//        LinkedList<String>s=new LinkedList<String>();
+//        s.add("Butterscotch");
+//        s.addFirst("Choclate");
+//        s.addLast("Vennela");
+//        s.add(4,"Ismart Sankar");
+//        s.remove();
+//        System.out.println(s);
+//    }
+//
+//}
+//import java.util.*;
+//
+//public class LinkedListExample {
+//    public static void main(String[] args) {
+//
+//        LinkedList<String> list = new LinkedList();
+//        list.add("A");
+//        list.add("B");
+//
+//        Iterator<String> it = list.iterator();
+//
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+//    }
+//}
+
+
--- src/main/java/org/example/MethodOverloading.java
+++ src/main/java/org/example/MethodOverloading.java
@@ -1,20 +1,83 @@
 package org.example;
 
-class math{
-    public void static int add(int a, int b){
-        return a+b;
-    }
+//class math{
+//    public static int add(int a, int b){
+//        return a+b;
+//    }
+//
+//    public double add(double a, double b,double c){
+//        return a+b+c;
+//        double d=10.4;
+//    }
+//}
+//
+//public class MethodOverloading {
+//    public static void main(String[] args) {
+//        System.out.println(math.add(11,10));
+//        System.out.println(math.add(11,12));
+//
+//
+//    }
+//}
 
-    public double add(double a, double b,double c){
-        return a+b+c;
-    }
-}
 
-public class MethodOverloading {
-    public static void main(String[] args) {
-        System.out.println(math.add(11,10));
-        System.out.println(math.add(11.5,12));
+//
+//class Adder {
+//    // Method to add two integers and return an integer
+//    static int add(int a, int b) {
+//        return a + b;
+//    }
+//    // Method to add two integers and return a double
+//    static double add(int a, int b) {
+//        return a + b;
+//    }
+//}
+//public class MethodOverloading {
+//    public static void main(String[] args) {
+//        // This line of code will cause ambiguity because both add methods have the same signature
+//        System.out.println(Adder.add(11, 11)); // Error: ambiguity
+//    }
+//}  MethodOverloading error occured
+
+//
+//class A {
+//    void print() {
+//        System.out.println("Control");
+//    };
+//    @Override
+//    void print(){
+//        System.out.println("MethodOverloading");
+//    };
+//}
+//public class MethodOverloading {
+//    public static void main(String[] args) {
+//        A a =new A();
+//        a.print();
+//    }
+//}
 
 
-    }
-}
+//class A{
+//    void display(){
+//    class B extends A{
+//        void text(){
+//            System.out.println("hello world");
+//        }
+//    }
+//}
+//public class MethodOverloading {
+//    public static void main(String[] args) {
+//
+////        A a=new A();
+////        A.B a1=a.new B();
+//        B b=new B();
+//
+//
+//    }
+//}
+
+
+
+
+
+
