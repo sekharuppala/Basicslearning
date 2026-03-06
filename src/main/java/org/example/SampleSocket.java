26c6830ebfc193ae57da4a8d612ebd7f7986c70a
Sekhar <sekhargowd645@gmail.com>
2026-03-06 22:43:44 +0530

    Update Chanages

--- src/main/java/org/example/SampleSocket.java
+++ src/main/java/org/example/SampleSocket.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -27,21 +27,93 @@ import java.net.UnknownHostException;
 //    }
 //}
 
-public class SampleSocket {
-    public static void main(String[] args) {
-        try {
-            Socket s = new Socket("localhost", 8234);
-            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-            dos.writeUTF("sekhar");
-            dos.writeUTF("25");
-            dos.writeInt(124);
-            dos.writeDouble(133.22);
-            dos.close();
-            s.close();
+//public class SampleSocket {
+//    public static void main(String[] args) {
+//        try {
+//            Socket s = new Socket("localhost", 8234);
+//            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+//            dos.writeUTF("sekhar");
+//            dos.writeUTF("25");
+//            dos.writeInt(124);
+//            dos.writeDouble(133.22);
+//            dos.close();
+//            s.close();
+//
+//        } catch (Exception e) {
+//            System.out.println(e);
+//
+//        }
+//    }
+//}
 
-        } catch (Exception e) {
-            System.out.println(e);
+//public class SampleSocket {
+//    public static void main(String... args) {
+//        try {
+//
+//            Socket s = new Socket("localhost",7123);
+//            DataInputStream dis=new DataInputStream(s.getInputStream());
+//            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+//            BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+//            String msg =" "; String msg2="sekhar";
+//            while(!msg.equals("stop")){
+//                msg.br.readLine();
+//                dos.writeUTF(msg);
+//                dos.flush();
+//                msg2=dos.readUTF();
+//                System.out.println(msg2);
+//            }
+//            dos.close();
+//            s.close();
+//
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//    }
+//}
 
-        }
-    }
-}
+//public class SampleSocket {
+//    public static void main(String[] args) throws Exception{
+//        Socket s=new Socket("localhost",3333);
+//        DataInputStream din=new DataInputStream(s.getInputStream());
+//        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+//        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//
+//        String str="",str2="";
+//        while(!str.equals("stop")){
+//            str=br.readLine();
+//            dout.writeUTF(str);
+//            dout.flush();
+////            System.out.println("Clent says:"+str);
+//            str2=din.readUTF();
+//            System.out.println("Server says: "+str2);
+//        }
+//
+//        dout.close();
+//        s.close();
+//
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
