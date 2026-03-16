f8a337a8e7a20664668ecb705e1bbfe790dbe45e
Sekhar <sekhargowd645@gmail.com>
2026-03-16 14:22:47 +0530

    stream comments

--- src/main/java/org/example/StreamsExample.java
+++ src/main/java/org/example/StreamsExample.java
@@ -3,6 +3,7 @@ package org.example;
 public class StreamsExample {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+        System.out.println("Java is object oriented language");
 
     }
 
