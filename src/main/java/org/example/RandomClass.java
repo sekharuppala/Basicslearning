52c362f259328cd9cdb126ee40eafd00fcee8ce9
Sekhar <sekhargowd645@gmail.com>
2026-03-20 12:43:32 +0530

    Random class commit

--- src/main/java/org/example/RandomClass.java
+++ src/main/java/org/example/RandomClass.java
@@ -1,37 +1,46 @@
-package org.example;
-
-import java.util.Random;
-
-public class RandomClass {
-    public static void main(String[] args) {
-        new RandomClass().Samples();
-
-    }
-
-    void Samples() {
-        Random r = new Random();
-        System.out.println(r.nextInt());
-//        System.out.println(r.nextFloat());
-//        System.out.println(r.nextBoolean());
-//        System.out.println(r.nextDouble());
-
-        System.out.println("Divider");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(r.nextInt());
-        }
-        System.out.println("Divider");
+//package org.example;
+//
+//import java.util.Random;
+//
+/////  float range = 32 bit
+///// / int range = 32 bit
+/////  byte range = 8 bit
+/////  short range = 16 bit
+///// long range = 64bit
+/////   double range = 64 bit
+//
+//public class RandomClass {
+//    public static void main(String[] args) {
+//        new RandomClass().Samples();
+//
+//    }
+//
+//    void Samples() {
+//        Random r = new Random();
+//        System.out.println(r.nextInt());
+////        System.out.println(r.nextFloat());
+////        System.out.println(r.nextBoolean());
+////        System.out.println(r.nextDouble());
+//
+//        System.out.println("Divider");
+//
+////        for (int i = 0; i < 10; i++) {
+////            System.out.println(r.nextInt());
+////        }
+////        System.out.println("Divider");
 //
 //        for (int i = 0; i < 10; i++) {
 //            System.out.println(r.nextInt(100000, 999999));
 //        }
 //        System.out.println("Divider");
-
-//        String[] names = new String[]{"Sekhar,Bhanu,Nag"};
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(names[new Random().nextInt(0, names.length)]);
-        }
-    }
-
-//}
-
+//
+////        String[] names = new String[]{"Sekhar,Bhanu,Nag"};
+////        for (int i = 0; i < 10; i++) {
+////            System.out.println(names[new Random().nextInt(0, names.length)]);
+//
+//        }
+//    }
+//
+////}
+//
+//
